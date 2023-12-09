bc47699399db76ab3994c9747a05b86edf95c82b
rjriver <152370211+rjriver@users.noreply.github.com>
2023-12-09 12:19:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,3 +35,33 @@ with st.container():
         Sometimes, we lose the game, and we will discuss our weaknesses together because we all believe the next time we can play better.
         """)
 
+
+ 
+
+st.header(":mailbox: Get In Touch With Me!")
+
+contact_form = """
+<form action="https://formsubmit.co/rrivera3@ssct.edu.ph" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+    <input type="text" name="name" placeholder="your name" required>
+    <input type="email" name="email" placeholder="your email" required>
+    <textarea name="message" placeholder="your message here"></textarea>
+    <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+local_css("pages/style/style.css")
+  
+with st.container():
+    st.write("---")
+ 
+
+
+
